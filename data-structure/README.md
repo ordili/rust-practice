88ea2db3ba67c94f39139b1c77a5ee2efb00eb89
ordili <qinqiqili@163.com>
2024-07-27 00:17:37 +0800

    modify README.md

---
+++ data-structure/README.md
@@ -0,0 +1,6 @@
+
+# Linkedlist
+利用到以下知识点
+1. NonNull
+2. Box::into_raw 把Box<T>转变未T的指针 *mut T
+3. Box::from_raw 把指针 *mut T 转变未Box<T>
--- data-structure/src/list.rs
+++ data-structure/src/list.rs
@@ -23,6 +23,7 @@ impl<T> LinkedList<T> {
         Self { len: 0, head: None }
     }
 
+    // 在链表头部插入节点
     pub fn insert_head(&mut self, t: T) {
         let mut node = Box::new(Node::new(t));
         node.next = self.head;
@@ -30,6 +31,7 @@ impl<T> LinkedList<T> {
         self.len += 1;
     }
 
+    // 删除链表头节点
     pub fn remove_head(&mut self) -> Option<T> {
         match self.head {
             Some(head) => unsafe {
@@ -41,20 +43,23 @@ impl<T> LinkedList<T> {
             _ => None,
         }
     }
-}
 
+    pub fn len(&self) -> usize {
+        self.len
+    }
+}
 #[cfg(test)]
 mod test {
     use crate::list::LinkedList;
     #[test]
     pub fn test_insert_remove() {
         let mut list = LinkedList::new();
-        list.insert_head(1);
-        list.insert_head(2);
-        list.insert_head(3);
-        assert_eq!(list.remove_head(), Some(3));
-        assert_eq!(list.remove_head(), Some(2));
-        assert_eq!(list.remove_head(), Some(1));
-        assert_eq!(list.remove_head(), None);
+        for v in 0..=10 {
+            list.insert_head(v);
+            assert_eq!(list.len(), v + 1);
+        }
+        for v in 10..=0 {
+            assert_eq!(list.remove_head(), Some(v));
+        }
     }
 }
