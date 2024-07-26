e12fb26ca0eed2f2be05501a857a64d0c92647da
ordili <qinqiqili@163.com>
2024-07-26 21:58:22 +0800

    change package name

--- mini-kvdb/src/main.rs
+++ mini-kvdb/src/main.rs
@@ -1,10 +1,9 @@
 use std::path::PathBuf;
-use mini_bitcask_kvdb::kvdb::MiniKVDB;
+use mini_kvdb::kvdb::MiniKVDB;
 
 fn main() {
     let path = PathBuf::from("mini.db");
     let mut db = MiniKVDB::new(path).unwrap();
-
     let v = vec![10,20,30,40,50];
     let key = b"key1";
     let _ = db.set(&key[..], v.clone());
