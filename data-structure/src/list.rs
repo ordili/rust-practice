b23a998b5fd6cf42611a34522a984206ea3c58aa
ordili <qinqiqili@163.com>
2024-07-27 00:53:54 +0800

    add insert_at method

--- data-structure/src/list.rs
+++ data-structure/src/list.rs
@@ -44,6 +44,39 @@ impl<T> LinkedList<T> {
         }
     }
 
+    // insert at ith
+    pub fn insert_at(&mut self, index: usize, t: T) {
+        let index = if index > self.len { self.len } else { index };
+        if index == 0 {
+            self.insert_head(t);
+            return;
+        }
+
+        let mut i = 0;
+        let mut head = self.head;
+        while i < index - 1 {
+            match head {
+                Some(head_ptr) => unsafe {
+                    let node = Box::from_raw(head_ptr.as_ptr());
+                    head = node.next;
+                    i += 1;
+                },
+                _ => break,
+            }
+        }
+
+        let mut new_node = Box::new(Node::new(t));
+        match head {
+            Some(head_ptr) => unsafe {
+                let mut node = Box::from_raw(head_ptr.as_ptr());
+                new_node.next = node.next;
+                (*head_ptr.as_ptr()).next = NonNull::new(Box::into_raw(new_node));
+                self.len += 1;
+            },
+            _ => {}
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.len
     }
@@ -61,5 +94,16 @@ mod test {
         for v in 10..=0 {
             assert_eq!(list.remove_head(), Some(v));
         }
+    }
+
+    #[test]
+    pub fn test_insert_at() {
+        let mut list = LinkedList::new();
+        list.insert_head(1);
+        list.insert_at(0, 10);
+        assert_eq!(list.remove_head(), Some(10));
+
+        list.insert_at(1, 10);
+        assert_eq!(list.remove_head(), Some(1));
     }
 }
